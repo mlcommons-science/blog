20de58166e50611c84dca3edf5ef9cdc550da0c9
Gregor von Laszewski <laszewski@gmail.com>
2023-03-13 19:54:08 -0400

    update docs

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "MLCommons Science",
+	"url": "https://mlcommons-science.netlify.app/",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
+	"description": "The MLCommons Science Blog.",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "MLCommons",
+		"email": "laszewski@gmail.com",
+		"url": "https://github.com/mlcommons/science"
 	}
 }
